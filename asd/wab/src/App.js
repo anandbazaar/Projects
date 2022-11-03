9727aaa11b9aa587f48a2f07838097877c5ce0a0
qndy <qndy@qndys-MacBook-Pro.local>
2022-11-03 15:36:51 +0800

    asd

--- asd/wab/src/App.js
+++ asd/wab/src/App.js
@@ -2,11 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Review } from "./components";
 
+
+
+
 function App() {
   return (
     <div className="App">
       <Review
         numStar={3}
+        name ='andy'
         text="Give everyone you work with—inside and outside your emoji, keep conversations focused in channels, and simplify all your communication into one place."
       ></Review>
     </div>
--- asd/wab/src/components/Review.js
+++ asd/wab/src/components/Review.js
@@ -1,86 +1,19 @@
+import { uuid } from 'uuidv4';
 export const Review = (props) => {
-  let asd;
-  switch (props.numStar) {
-    case 0:
-      asd = (
-        <div>
-          {" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>
-        </div>
-      );
-      break;
-    case 1:
-      asd = (
-        <div>
-          {" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>
-        </div>
-      );
-      break;
-    case 2:
-      asd = (
-        <div>
-          {" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>
-        </div>
-      );
-      break;
-    case 3:
-      asd = (
-        <div>
-          {" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star"></span>{" "}
-          <span className="fa fa-star"></span>
-        </div>
-      );
-      break;
-    case 4:
-      asd = (
-        <div>
-          {" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star"></span>
-        </div>
-      );
-      break;
-    case 5:
-      asd = (
-        <div>
-          {" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>{" "}
-          <span className="fa fa-star checked"></span>
-        </div>
-      );
-  }
-  new Array(5).map((x) => {
-    console.log("asd");
+const asd = Array.apply(null, Array(5)).map((x,i) => {
+    return(
+      i <= props.numStar ? <span className="fa fa-star checked"></span> : <span className="fa fa-star"></span>
+    )
   });
 
   return (
     <div className="rot">
       <div className="rot1">{asd}</div>
       <div className="rot2">{props.text}</div>
+      <div className="rot3">
+        <div className='photoBooth'><img src = '../asd.jpeg'></img></div>
+        <p className='name'>{props.name}</p>
+      </div>
     </div>
   );
 };
