653accd93bf049f93128f1990723428cfe65d8c7
qndy <qndy@qndys-MacBook-Pro.local>
2022-11-12 10:49:32 +0800

    asd

--- asd/wab/src/components/Review.js
+++ asd/wab/src/components/Review.js
@@ -1,5 +1,8 @@
 import { uuid } from "uuidv4";
 import styles from "../styles/Review.module.css";
 export const Review = (props) => {
-  return <div className={styles.Review}></div>;
+  return <div className={styles.Review}>
+    {console.log(props.starCount)}
+    {props.starCount}
+  </div>;
 };
--- asd/wab/src/pages/Home.js
+++ asd/wab/src/pages/Home.js
@@ -1,16 +1,20 @@
 import style from "../styles/main.module.css";
+import data from "../data/reviewData.json";
 import { useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 import { Footer, Review } from "../components/index";
 import { Prev } from "../../node_modules/react-bootstrap/esm/PageItem";
+import { v4 as uuidv4 } from "uuid";
+
 const goRight = (props) => {
   props[1](props[0] + 1);
 };
 export const Home = (props) => {
+
   const [index, IndexC] = useState(0);
   const [ReviewCount, ReviewCountChange] = useState(
-    Array.apply(null, { length: 10 })
+    Array.apply(null, { length: 5 })
   );
   const [inputText, inputTextChanger] = useState("");
   return (
@@ -43,12 +47,12 @@ export const Home = (props) => {
         <div
           className={style.ReviewCont}
           style={{
-            transform: `translateX(${-10 * index}% `,
+            transform: `translateX(${-(100 / ReviewCount.length) * index}% `,
             transition: "300ms",
           }}
         >
           {ReviewCount.map((x, i) => {
-            return <Review />;
+            return <Review key={uuidv4()} starCount = {null} />;
           })}
         </div>
         <Button
@@ -56,7 +60,7 @@ export const Home = (props) => {
             goRight([index, IndexC]);
           }}
           className={style.forward}
-        ></Button>
+        ></Button>  
       </div>
       <Footer />
     </div>
