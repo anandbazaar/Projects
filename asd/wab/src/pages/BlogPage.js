1b1bcb6761cd8fab6380d76987aedc31f7271ca8
qndy <qndy@qndys-MacBook-Pro.local>
2022-11-17 15:31:33 +0800

    asd

--- asd/wab/src/pages/BlogPage.js
+++ asd/wab/src/pages/BlogPage.js
@@ -4,10 +4,18 @@ import { Blog } from "../components/Blog";
 import styles from "../styles/Blogs.module.css";
 import data from "../data/blogs.json";
 import axios from "../../node_modules/axios/index";
+import Button from "../../node_modules/react-bootstrap/esm/Button";
 import Spinner from "../../node_modules/react-bootstrap/esm/Spinner";
+import { Prev } from "../../node_modules/react-bootstrap/esm/PageItem";
 export const BlogPage = (props) => {
   const [page, pageChange] = useState(0);
-  const [limit, limitChange] = useState(10);
+  const nextPage = () =>{
+    pageChange(page+1)
+  }
+  const lastPage = () =>{
+    pageChange(page-1)
+  }
+  const [limit, limitChange] = useState(8);
   const [datar, datarChange] = useState(null);
   const baseUrl = "https://dummyapi.io/data/v1/";
   useEffect(() => {
@@ -31,7 +39,6 @@ export const BlogPage = (props) => {
       <h1>Blog posts</h1>
       <p> our latest updates and blogs about managing our team</p>
       <div className={styles.blogCont}>
-        <button></button>
         {datar !== null ? (
           datar.map((x, i) => {
             return (
@@ -49,6 +56,8 @@ export const BlogPage = (props) => {
           <Spinner></Spinner>
         )}
       </div>
+      <Button onClick={()=>{nextPage()}} className={styles.bu}>next</Button>
+      <Button onClick={()=>{lastPage()}} className={styles.bu}>back</Button>
     </div>
   );
 };
