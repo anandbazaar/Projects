974759daa622b66bdb0e09838ae712bfebf92012
anand <anand@nest-101.local>
2022-11-22 19:36:56 +0800

    asd

--- spotify/spotify/src/components/Context.js
+++ spotify/spotify/src/components/Context.js
@@ -1,14 +1,19 @@
-import { Children, createContext, useState } from "react";
+import { Children, createContext, useContext, useState } from "react";
 
 export const HiddenContext = createContext({});
-export const HiddenProvider = ({children}) =>{
-    const [hidden, stateChange] = useState(true)
-const changeState = () =>{
-    stateChange(!hidden)
-}
-return(
-    <HiddenContext.Provider value={{hidden,changeState}}>
-        {children}
+
+export const HiddenProvider = ({ children }) => {
+  const [hidden, setHidden] = useState(true);
+
+  const changeState = () => setHidden((prev) => !prev);
+
+  const hide = () => setHidden(true);
+
+  return (
+    <HiddenContext.Provider value={{ hidden, changeState, hide }}>
+      {children}
     </HiddenContext.Provider>
-)
-}
+  );
+};
+
+export const useHidden = () => useContext(HiddenContext);
--- spotify/spotify/src/components/DJ.js
+++ spotify/spotify/src/components/DJ.js
@@ -1,41 +1,94 @@
-import { useContext, useRef, useState } from "react"
-import styles from "../styles/Dj.module.css"
-import { HiddenContext } from "./Context";
+import { useEffect, useRef, useState } from "react";
+import styles from "../styles/Dj.module.css";
+import { useHidden } from "./Context";
 import { ProgressBar } from "./ProgressBar";
+import axios from "../../node_modules/axios/index";
+
+const baseurl = "https://spotify-scraper.p.rapidapi.com/v1/track/download";
+
 export const Dj = (props) => {
-    const  [isPlaying,changePlaying] = useState(false)
-    const { hidden, changeState } = useContext(HiddenContext);
-    const sideBar = useRef();
-    const But = useRef();
-    return(
-        <div ref={sideBar} className={styles.hidden}>
-            <button ref={But} className={hidden === true ? styles.sideBarClosed : styles.sideBarOpened} onClick={()=>{
-                
-                console.log(sideBar.current.className)
-                   if(sideBar.current.className === 'Dj_hidden__f3LVJ'){
-                    changeState();
-                    sideBar.current.className = styles.exposed
-                    props.back.current.className = 'App_innerBack__yeCog'
-                    props.back.current.style.backgroundImage = "url('https://i.ytimg.com/vi/K09_5IsgGe8/maxresdefault.jpg')";
-                    return           
-                   }
-                   if(sideBar.current.className === 'Dj_exposed__bDD+s'){
-                    changePlaying(!isPlaying)
-                    But.current.style.animationPlayState =  !isPlaying ? 'running' : 'paused'
-                   }
-            }}>
-                
-                <div className={hidden === true ? styles.innerClosed : styles.innerOpened}>
-                    <img className={styles.songCover} src={props.songImg}></img>
-                    <div className={styles.pin}></div>
-                </div>
-               
-            </button>
-            <p className={styles.title}>{props.songTitle}</p>
-            <ProgressBar play={[isPlaying,changePlaying]} ch={sideBar}></ProgressBar>
-            <div className={styles.control}>
-                <button style={{backgroundColor:"transparent",border:"none",}}><i className="fa fa-pause" style={{fontSize: "20px",color:"white"}}></i></button>
-            </div>
+  const { hidden, changeState } = useHidden();
+  const [audio, setAudio] = useState(null);
+  const [data, setData] = useState(null);
+  const [keyWord, keyWordChange] = useState("");
+  const [search, setSearch] = useState("");
+  const [isPlaying, changePlaying] = useState(false);
+
+  const clickHandler = () => {
+    if (hidden) {
+      changeState();
+    } else if (data) {
+      changePlaying((prev) => !prev);
+    }
+  };
+
+  useEffect(() => {
+    if (isPlaying && audio) {
+      audio.play();
+    } else if (audio) {
+      audio.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    axios
+      .get(baseurl + `?track=` + keyWord, {
+        headers: {
+          "X-RapidAPI-Key":
+            "909a9752a2msh0b7f47cf1952823p1e4077jsn97ecb5da73e7",
+          "X-RapidAPI-Host": "spotify-scraper.p.rapidapi.com",
+        },
+      })
+      .then((res) => {
+        setData(res.data);
+        setAudio(new Audio(res.data.youtubeVideo.audio[0].url));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [keyWord]);
+
+  return (
+    <div className={hidden ? styles.hidden : styles.exposed}>
+      <button
+        style={{ animationPlayState: isPlaying ? "running" : "paused" }}
+        className={hidden ? styles.sideBarClosed : styles.sideBarOpened}
+        onClick={clickHandler}
+      >
+        <div className={hidden ? styles.innerClosed : styles.innerOpened}>
+          <img
+            className={styles.songCover}
+            src={data ? data.spotifyTrack.album.cover[0].url : undefined}
+          ></img>
+          <div className={styles.pin}></div>
         </div>
-    )
-}
+      </button>
+
+      <input
+        value={search}
+        onKeyDown={(e) => {
+          if (e.keyCode === 13) {
+            keyWordChange(e.target.value);
+            return;
+          }
+          setSearch((prev) => prev + e.key);
+        }}
+        placeholder="Search"
+        className={hidden ? styles.gno : styles.search}
+      >
+        {props.title}
+      </input>
+
+      <ProgressBar play={[isPlaying, changePlaying]} />
+
+      <div className={styles.control}>
+        <button style={{ backgroundColor: "transparent", border: "none" }}>
+          <i
+            className="fa fa-pause"
+            style={{ fontSize: "20px", color: "white" }}
+          ></i>
+        </button>
+      </div>
+    </div>
+  );
+};
